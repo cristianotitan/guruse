3c12c0308551ffd5ff56bba2dbbe00665a1ed7e0
Haroldo Jardim Furtado de Mendonça <haroldofurtado@gmail.com>
2013-11-09 02:19:08 -0300

    Criação do rodapé e criando o Quem somos?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Guruse2::Application.routes.draw do
   resources :meetings
+  resources :about_us
 
   root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
